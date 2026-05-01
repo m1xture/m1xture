7657b52496380f7144a3bb1ed8258196ad99f40e
m1xture <m1xture.dev@gmail.com>
2026-05-01 16:17:30 +0300

    added astro/netlify provider

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
-
+import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
